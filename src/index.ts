789db3ea05f3c9e2d7310bf860f769c546c46f35
Corey Connor <corey.connor@oceanpartners.com>
2021-10-06 20:29:56 +0000

    configured bootstrapping a connection to mongoDB with typeorm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('Hello World');
+import { Database } from './schemas/database';
+
+const MongoDB = new Database({
+  type: "mongodb",
+  host: "localhost",
+  port: 27017,
+  database: "amity-systems"
+});
+
+// Initialize connection to mongoDB
+(async () => {
+  await MongoDB.connect();
+})()
+
+// When node attempt to gracefully shutdown the process.
+process.on('SIGINT', () => {
+  (async () => {
+    let err = await MongoDB.close();
+    process.exit((err) ? 1 : 0);
+  })();
+});
